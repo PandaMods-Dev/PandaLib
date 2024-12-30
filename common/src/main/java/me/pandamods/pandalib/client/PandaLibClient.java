05cd58c30f1f75fd3dd9c214a9d129dc6b07b772
The Panda Oliver <Oliver.h.froberg@gmail.com>
2024-12-30 17:03:52 +0100

    Remove animation and rendering-related classes.
    This commit deletes the animation handling and model rendering classes, including `AnimationController`, `Animatable`, and `ModelRenderer`. This simplifies the codebase by eliminating unused or deprecated functionality related to animations and rendering.

--- common/src/main/java/me/pandamods/pandalib/client/PandaLibClient.java
+++ common/src/main/java/me/pandamods/pandalib/client/PandaLibClient.java
@@ -15,7 +15,6 @@ package me.pandamods.pandalib.client;
 import me.pandamods.pandalib.PandaLib;
 import me.pandamods.pandalib.core.client.event.EventHandlerClient;
 import me.pandamods.pandalib.registry.ReloadListenerRegistry;
-import me.pandamods.pandalib.resource.AssimpResources;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.server.packs.PackType;
@@ -23,7 +22,6 @@ import net.minecraft.server.packs.PackType;
 @Environment(EnvType.CLIENT)
 public class PandaLibClient {
     public PandaLibClient() {
-		ReloadListenerRegistry.register(PackType.CLIENT_RESOURCES, new AssimpResources(), PandaLib.resourceLocation("assimp_loader"));
 		EventHandlerClient.init();
     }
 }
